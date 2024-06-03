25e5bca2d29ab3b089d00574d7621d335ae32718
Annu Kumari <108427028+Annu117@users.noreply.github.com>
2024-06-03 06:24:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["https://mern-api-orcin.vercel.app/"],
+    origin: ["https://mern-frontend-lac.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true
   }
